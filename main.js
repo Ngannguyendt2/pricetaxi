21a0d02cbb79ba0d360515df7d04bc59eb181aac
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-28 15:23:00 +0700

    repair

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ let km = Number(prompt("Import km"));
 function priceTaxi() {
     if (km <= 1) {
         price = 5000;
-    } else if ((km > 1) && (km <= 30)) {
-        price = km * 4000;
+    } else if ((km >= 1) && (km <= 30)) {
+        price = (km - 1) * 4000 + 5000;
     } else {
-        price = km * 3000;
+        price = (km - 31) * 3000 + (km - 1) * 4000 + 5000;
     }
     return price;
 }
